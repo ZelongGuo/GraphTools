8485d128dce678f0eb383f6b500e5c827ad416ed
Zelong Guo <zelong.guo@outlook.com>
2023-06-29 18:31:22 +0200

    update

--- aftershock_profile/get_aftershock_density.py
+++ aftershock_profile/get_aftershock_density.py
@@ -10,12 +10,16 @@ import aftershock_profile as ap
 import os
 import numpy as np
 
+# -----------------------------------------------------------------
 # now we select the aftershocks within in the swath
 start_lon, start_lat = 45.325, 34.247
 end_lon, end_lat = 46.082, 35.129
 start_point = np.array([start_lon, start_lat])
 end_point = np.array([end_lon, end_lat])
 swath_width = 8  # unit is km  # the total width of the swath
+# note the distance_max and depth_max you should also change them maybe
+# -----------------------------------------------------------------
+
 
 # combine the aftershock files together: redirect to a new file: lon lat depth ...
 os.system("grep '^[0-9]' ./Fathian_aftershocks.txt | awk -F ' ' '{print $1, $2, $3}' > all_relocated_eq.txt")
